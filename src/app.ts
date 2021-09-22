b01e6d2c53f64cfa4a7400a729c44735846ea647
hpratt <hpratt111@live.com>
2021-09-22 14:16:00 -0400

    try bumping JSON POST size limit

--- src/app.ts
+++ src/app.ts
@@ -39,6 +39,8 @@ const createApp = (services: Service[], addMiddleware: AddMiddleware): Express =
     const app = express();
 
     app.use(compression());
+    app.use(express.json({ limit: '25mb' }));
+    app.use(express.urlencoded({ limit: '25mb' }));
 
     const port = process.env.PORT || 3000;
     app.set("port", port);
